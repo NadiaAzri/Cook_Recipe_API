34c30d8db83aed09dc3a5e1c3b857667f623993e
Nadia Azri <azrinadia79@gmail.com>
2021-11-08 20:12:02 +0100

    updated urls

--- recipe/urls.py
+++ recipe/urls.py
@@ -7,13 +7,13 @@ from rest_framework.authtoken.views import obtain_auth_token
 router = DefaultRouter()
 router.register('recipe', views.ViewsetsRecipe, basename='recipes')
 router2 = DefaultRouter()
-router2.register('comments', views.ViewsetsComment, basename='comments')
+router2.register('comment', views.ViewsetsComment, basename='comments')
 router3 = DefaultRouter()
-router3.register('replies', views.ViewsetsReply, basename='replies')
+router3.register('reply', views.ViewsetsReply, basename='replies')
 
 urlpatterns = [
     # Viewsets
-    path('cook/recipe/<str:recipe_pk>/comments/<str:parent_pk>/', include(router3.urls)),
+    path('cook/recipe/<str:recipe_pk>/comment/<str:parent_pk>/', include(router3.urls)),
     path('cook/recipe/<str:recipe_pk>/', include(router2.urls)),
     path('cook/', include(router.urls)),
 ]
